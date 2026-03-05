79ae8925fe890d110a2718d675d393589c325e58
mousabmt <mousabtamari0799@gmail.com>
2026-03-05 17:05:42 +0300

    Login Page

---
+++ unidesk/lib/core/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryGold = Color(0xFFFFDF00);
+  static const primaryBlue = Color(0xFF00B1C1);
+  static const background = Color(0xFF0F172A);
+  static const hint = Color(0xFF9CA3AF);
+}
